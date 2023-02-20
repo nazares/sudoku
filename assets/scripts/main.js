73a34fc13ebcfed41055c384fdb47bbdc6b3bfdd
Sergei Nazarenko <nazares@icloud.com>
2023-02-20 20:48:41 +0300

    add font, favicon, styles, scripts

---
+++ assets/scripts/main.js
@@ -0,0 +1,21 @@
+class Sudoku extends HTMLElement {
+    constructor() {
+        super();
+        this.divElement = null;
+        this.shadow = this.attachShadow({ mode: "open" });
+        this.render();
+    }
+    render() {
+        this.shadow.innerHTML = `
+        <link rel="stylesheet" href="assets/styles/style.css">`;
+        for (let i = 0; i <= 80; ++i) {
+            this.divElement = document.createElement("div");
+            this.divElement.classList.add("main-grid-cell");
+            this.divElement.innerHTML = i;
+            this.shadow.appendChild(this.divElement);
+        }
+
+    }
+}
+
+customElements.define('sudoku-board', Sudoku);
